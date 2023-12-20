cb0f91573496207c84893ba8c646bde83e1ff5e1
MahfuzurShakil <shakil.mahfuzur@gmail.com>
2023-12-20 16:02:32 +0600

    While and do while loop, Continue and break Statements.

--- src/ControlFlow/ForWhileDoWhileLoop.java
+++ src/ControlFlow/ForWhileDoWhileLoop.java
@@ -1,5 +1,7 @@
 package ControlFlow;
 
+import java.util.Scanner;
+
 public class ForWhileDoWhileLoop {
     public static void main(String[] args) {
         for (int i=0; i<5; i++){
@@ -9,5 +11,20 @@ public class ForWhileDoWhileLoop {
         for (int i=5; i>0; i--){
             System.out.println(i);
         }
+
+        //When we don't know how many times we need run statements, should use While loop
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while (!input.equals("quit")){
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+
+        }
+
+        do{
+            System.out.println("GG");
+        }while (!input.equals("quit"));
     }
 }
